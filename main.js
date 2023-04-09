010f12b1b4f4070cc32adf0a8623309bc9aba29e
el_raccoon <agrian142476@gmail.com>
2023-04-09 03:46:09 -0400

    tiny: added prefix actions + i am very high

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ const { disc_bot_token } = require('./c.json');
 // new instance
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-// client log
+// bot log on start
 client.once("ready", () => {
   console.log("ShaqBot up & running!")
+})
+
+// bot actions
+const prefix = ";"
+client.on("message", message => {
+  if (!message.content.startsWith(prefix) || message.author.bot)
+    return;
+  const args = message.content.slice(prefix.length).split(/ +/);
 })
 
 // login using token
