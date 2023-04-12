490880a535318ca0ba9a341c093c78071cd5ed16
unknown <carlow00x@gmail.com>
2023-04-11 23:31:20 -0400

    Changes: added  intents ,modified messageCreate

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { disc_bot_token } = require('./c.json');
 
 // new instance
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
 // bot log on start
 client.once("ready", () => {
@@ -11,10 +11,29 @@ client.once("ready", () => {
 
 // bot actions
 const prefix = ";"
-client.on("message", message => {
+client.on("messageCreate", message => {
+  console.log("message is recieved");
   if (!message.content.startsWith(prefix) || message.author.bot)
+  {
+    console.log("message does not start with prefix");
     return;
+  }
+
+
   const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping')
+  {
+      //const channel = client.channels.cache.get('💦-cum-zone-general')
+      message.channel.send('pong');
+  }
+  else if(command == 'bruh')
+  {
+      message.channel.send('server');
+  }
+
+
 })
 
 // login using token
