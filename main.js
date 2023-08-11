a6c7fefa9382b6e990327c90f3ebc4be14862e20
unknown <carlow00x@gmail.com>
2023-08-10 23:34:34 -0400

    added the connection to the mongodb database for shaqbot

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { Client, Events, GatewayIntentBits, Collection, Partials } = require("discord.js");
-const { disc_bot_token } = require('./c.json');
-
+const { disc_bot_token, mongodb_srv } = require('./c.json');
+const mongoose = require("mongoose");
 // new client instance
 const client = new Client({intents: [
   GatewayIntentBits.Guilds, 
@@ -21,6 +21,17 @@ client.events = new Collection();
 ["command_handler", "event_handler"].forEach(handler =>{
   require(`./handlers/${handler}`)(client);
 })
+
+// MongoDB DataBase Connection
+mongoose.connect(mongodb_srv, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() =>{
+  console.log("connected to DB BND");
+}).catch((err) => {
+  console.log(err);
+});
+
 
 // login using token
 client.login(disc_bot_token)
