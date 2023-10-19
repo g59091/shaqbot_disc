5ee62056c512723c492bd0280a8bd0dfc4042b17
unknown <carlow00x@gmail.com>
2023-10-19 00:44:49 -0400

    refactored images and named them with canvas

--- commands/roll.js
+++ commands/roll.js
@@ -2,7 +2,6 @@
 // Second: grab 2-3 random cards from shaq_gacha_pics
 // Third: when user clicks on card, associate picture path with scards string array in schema
 //const shaqModel = require("../models/shaqschema");
-//const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
 const Canvas = require('@napi-rs/canvas');
 const path = require("path");
 const fs = require("fs");
@@ -13,33 +12,34 @@ module.exports = {
     permissions: [],
     cooldown: 300,
     description: ' lets the user roll for sCards ',
-
     async execute(message, args, client, profileInfo) {
         if (message.channel.name !== "🤖-commands") return message.channel.send("Please use this command in the 🤖-commands channel"); 
         const gachaDir = path.resolve() + path.sep + "media" + path.sep + "shaq_gacha_pics";
         const backgroundJpg = path.resolve() + path.sep + "media" + path.sep + "background_test.jpg";
         const gachaFiles = fs.readdirSync(gachaDir);
         const cardsDropped = gachaFiles.sort(() => Math.random() - Math.random()).slice(0, 3);
-        const sampleCanvas = Canvas.createCanvas(1300, 768);
+        const sampleCanvas = Canvas.createCanvas(1500, 600);
         const sampleContext = sampleCanvas.getContext("2d");
         const sampleBackground = await Canvas.loadImage(backgroundJpg);
         sampleContext.drawImage(sampleBackground, 0, 0, sampleCanvas.width, sampleCanvas.height);
         var cardCount = 0;
         for (const card of cardsDropped) {
           const cardImage = await Canvas.loadImage(gachaDir + path.sep + card);
-          sampleContext.drawImage(cardImage, cardCount + 100, 100, 100, cardCount + 100);
-          cardCount += 400;
+          // assume file name ends in .jpg or .png
+          const cardNoExt = card.slice(0, -4);
+          //const cardWRatio = cardImage.width / sampleCanvas.width;
+          //const cardHRatio = cardImage.height / sampleCanvas.height;
+          //sampleContext.drawImage(cardImage, 0, 0, cardImage.width, cardImage.height, cardCount + 100, 100, cardImage.width * cardRatio, cardImage.height * cardRatio);
+          //console.log(300 * cardWRatio);
+          sampleContext.drawImage(cardImage, cardCount + 100, 100, 400, 400);
+          sampleContext.fillStyle =  "white";
+          sampleContext.fillRect(cardCount + 100, 500, 400, 50);
+          sampleContext.fillStyle =  "black";
+          sampleContext.font = "40px Comic Sans MS";
+          sampleContext.fillText(cardNoExt, cardCount + 250, 540); 
+          cardCount += 450;
         }
-
         const sampleAttachment = new AttachmentBuilder(await sampleCanvas.encode("png"), {name: "background.png"});
         message.channel.send({files: [sampleAttachment]});
-        
-        /* 
-        const cardsSampleOne = new AttachmentBuilder(gachaDir + path.sep + cardsDropped[0]);
-        const cardsSampleTwo = new AttachmentBuilder(gachaDir + path.sep + cardsDropped[1]);
-        const cardsSampleThree = new AttachmentBuilder(gachaDir + path.sep + cardsDropped[2]);
-        message.channel.send({ files: [cardsSampleOne, cardsSampleTwo, cardsSampleThree] });
-       */
-
     }
 }
