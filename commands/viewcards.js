ec6bbe70d53dabd6e0c932810a49dcc99a22b47a
unknown <carlow00x@gmail.com>
2024-01-12 18:25:39 -0500

    Finished view cards command

--- commands/viewcards.js
+++ commands/viewcards.js
@@ -3,7 +3,7 @@ const path = require("path");
 const Canvas = require('@napi-rs/canvas');
 const fs = require("fs");
 const { AttachmentBuilder } = require('discord.js');
-const { card_game_effects } = require("../c.json");
+const { card_game_effects, card_game_quotes } = require("../c.json");
 module.exports = {
     name: 'view',
     aliases: [],
@@ -18,6 +18,9 @@ module.exports = {
         const backgroundJpg = path.resolve() + path.sep + "media" + path.sep + "background_portrait.jpg";
         const gachaFiles = fs.readdirSync(gachaDir); 
         var imageName = "";
+        var imageQuote = "N/A";
+        var shaqCardName = "";
+        var shaqCardEffect = "plain";
         var searchTerm = args[0];
         try { 
           const imageDoc = await shaqModel.findOne({ 
@@ -33,6 +36,11 @@ module.exports = {
           for (var file of gachaFiles) {
             if (file.slice(0, -4) == sCardname.split("_")[0]){
               imageName = file;
+              shaqCardName = file.slice(0, -4);
+              shaqCardEffect = sCardname.split("_")[1];
+              //console.log(card_game_quotes);
+              if (card_game_quotes.hasOwnProperty(shaqCardName))
+                imageQuote = card_game_quotes[shaqCardName];
             }
           }
           //console.log(imageName);
@@ -47,16 +55,21 @@ module.exports = {
         const sampleBorder = await Canvas.loadImage(borderPng);
         const cardImage = await Canvas.loadImage(gachaDir + path.sep + imageName);
         sampleContext.drawImage(sampleBackground, 0, 0, sampleCanvas.width, sampleCanvas.height);
-        shaqCardEffect = Math.random() <= 0.000001 ? "poop" : "plain";
+        //shaqCardEffect = Math.random() <= 0.000001 ? "poop" : "plain";
         cardEffectHelper(shaqCardEffect, sampleContext, cardImage);
-        sampleContext.fillStyle = "white";
-        sampleContext.fillRect(150, 950, 787, 500);
+        sampleContext.fillStyle = "#927024";
+        sampleContext.fillRect(50, 900, 987, 650);
+        sampleContext.fillStyle = "#e5cd96";
+        sampleContext.fillRect(150, 925, 787, 75);
+        sampleContext.fillRect(150, 1040, 787, 75);
+        sampleContext.fillRect(150, 1155, 787, 75);
+        sampleContext.fillRect(150, 1280, 787, 175);
         sampleContext.fillStyle =  "black";
         sampleContext.font = "40px Comic Sans MS";
-        sampleContext.fillText("card name", 175, 1000); 
-        sampleContext.fillText("card ID", 175, 1100);
-        sampleContext.fillText("card rarity", 175, 1200);
-        sampleContext.fillText("card quote", 175, 1300);
+        sampleContext.fillText("Card: " + shaqCardName, 175, 975); 
+        sampleContext.fillText("Print: " + searchTerm, 175, 1090);
+        sampleContext.fillText("Rarity: " + shaqCardEffect, 175, 1205);
+        sampleContext.fillText("\"" + imageQuote + "\"", 175, 1350);
         
         sampleContext.drawImage(sampleBorder, 0, 0, sampleCanvas.width, sampleCanvas.height);
       
@@ -66,20 +79,20 @@ module.exports = {
     }
 }
 const cardEffectHelper = (cEffect, cContext , cImage) => {
-//cContext.drawImage(cImage, cCount + 100, 100, 400, 400);
-const effectList = Object.getOwnPropertyNames(card_game_effects);
-//console.log(cEffect);
-switch(cEffect) {
+  //cContext.drawImage(cImage, cCount + 100, 100, 400, 400);
+  const effectList = Object.getOwnPropertyNames(card_game_effects);
+  //console.log(cEffect);
+  switch(cEffect) {
     //case effectList[0]: 
     //break;
     case effectList[1]:
-    //console.log("effect has been found");
-    cContext.drawImage(cImage, cCount + 100, 100, 400, 400);
-    cContext.fillStyle = "rgba(139, 69, 19, 0.65)";
-    cContext.fillRect(cCount + 100, 100, 400, 400);
+      //console.log("effect has been found");
+      cContext.drawImage(cImage, 50, 40, 987, 860);
+      cContext.fillStyle = "rgba(139, 69, 19, 0.65)";
+      cContext.fillRect(50, 40, 987, 860);
     break;
     default: 
-    cContext.drawImage(cImage,50, 40, 987, 860);
+      cContext.drawImage(cImage,50, 40, 987, 860);
   }
 } 
 
